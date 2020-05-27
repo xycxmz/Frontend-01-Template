6a4b8ffae44838bbbd80ff40dd24d438032ea3eb
xycxmz <401525991@qq.com>
2020-05-27 23:44:32 +0800

    Update server.js

--- week07/browser/server.js
+++ week07/browser/server.js
@@ -6,7 +6,37 @@ const server = http.createServer((req, res) => {
 	console.log(req.headers);
 	res.setHeader('Content-Type', 'text/html');
 	res.writeHead(200);
-	res.end("ok");
+	res.end(
+		(`
+	<html maaa=a >
+	<head>
+	<style>
+	#wrap {
+	  display: flex;
+	  width: 600px;
+	  height: 600px;
+	  background-color: #fff;
+	}
+	.item1 {
+	  width: 200px;
+	  height: 300px;
+	  background-color: #000;
+	}
+	.item12{
+	  flex: 1
+	  height: 400px;
+	  background-color: green;
+	}
+	</style>
+	</head>
+	<body>
+	  <div id="wrap">
+		<div class="item1">item1</div>
+		<div class="item2">item2</div>
+	  </div>
+	</body>
+	</html>
+	  `);
 	});
 
 server.listen(8080);
